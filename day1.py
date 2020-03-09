f6af6f31d39f5f7bf04da05b54d707211e2e832f
Wenzhe_Yang <41222624+JKYang01@users.noreply.github.com>
2020-03-09 04:53:04 -0400

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,5 +1,5 @@
 
-## find the maximum area 
+## find the maximum area  num:11 
 
 def maxArea(self, height: List[int]) -> int:
         area = 0 
@@ -38,3 +38,32 @@ def maxArea(self, height: List[int]) -> int:
                 r -= 1
 
         return area
+
+
+### move zero num: 283
+# [1,2,3,0,4,0,3,12] to [1,2,3,4,3,12,0,0]
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        lastzerofound = 0
+        for i in range(len(nums)):
+            if nums[i]!=0: # found none zero 
+                # exchange the position of zero to non-zero 
+                # if there are no 0 nums[i]=nums[lastzerofound]
+                # when the loop meets the zero, it will skip it and point to the next-non 0
+                # 'lastzerofound' keep the idex of 0 which next to the last non-0 
+                # because we set the condition if !=0 at 
+                # but the loop skip indx to non-0
+                nums[i], nums[lastzerofound]=nums[lastzerofound],nums[i]
+                print(nums[i])
+                print(nums[lastzerofound])
+                #print(lastzerofound)
+                lastzerofound +=1 # now is it i?
+                
+        return nums
+
+
