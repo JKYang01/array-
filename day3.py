f97c31ce17baa50ee15078e0a09847299d9b7962
Wenzhe_Yang <41222624+JKYang01@users.noreply.github.com>
2020-03-11 03:45:58 -0400

    Update day3.py

--- day3.py
+++ day3.py
@@ -16,6 +16,43 @@ A solution set is:
 
 
 ## others solution 
+## separate different conditon insimpler way 
+def threeSum(self, nums: List[int]) -> List[List[int]]:
+  nums.sort()
+  size = len(nums)
+  solution = []
+  
+  for idx in range(0, size-2):
+    # avoid repetition
+    if idx and nums[idx] == nums[idx-1]:
+      idx +=1
+      pass
+      continue
+      
+      first_num = nums[idx] 
+      target = -first_num
+      i, j = idx+1, size-1
+      while i < j:
+          cur_two_sum = nums[i] + nums[j]
+          if cur_two_sum > target:
+            j -=1
+          elif cur_two_sum < target:
+              i += 1
+          else:
+            solution.append( [ first_num, nums[i], nums[j] ] )
+            i += 1
+            j -= 1
+                    
+					# avoid repetition
+        while i < j and nums[i] == nums[i-1]:
+            i += 1
+                    
+					# avoid repetition
+        while i < j and nums[j] == nums[j+1]:
+            j -= 1
+                    
+    return solution
+
 # solution 1  using the math function and making a hash table with collection.counter()
 
 def threeSum(self, nums: List[int]) -> List[List[int]]:
@@ -38,59 +75,58 @@ def threeSum(self, nums: List[int]) -> List[List[int]]:
     # Hash set to store tuples of valid solutions.
     # Automatically handles duplicate solutions if we add
     # sorted tuples to it.
-        solutions = set()
+    solutions = set()
 
-        # Initializing our counter. O(n).
-        for i in nums:
-            if i < 0:
-                neg.setdefault(i, 0)
-                neg[i] += 1
-            elif i > 0:
-                pos.setdefault(i, 0)
-                pos[i] += 1
-            else:
-                zeros += 1
+# Initializing our counter. O(n).
+    for i in nums:
+        if i < 0:
+            neg.setdefault(i, 0)
+            neg[i] += 1
+        elif i > 0:
+            pos.setdefault(i, 0)
+            pos[i] += 1
+        else:
+            zeros += 1
 
         # We iterate through all unique values of nums.
         # Note: 'i' and 'j' are always opposite in parity. i.e. 'i'
         # and 'j' will never both be positive or both be negative.
-        for i in {num for num in nums}:
+    for i in {num for num in nums}:
             # Seeking positive numbers to offset negative numbers.
-            if i < 0:
-                for j in pos:
+         if i < 0:
+            for j in pos:
                     # Seek for third number, k = -(i + j) 
-                    k = -i - j
-                    if k in pos:
+                k = -i - j
+                if k in pos:
                         # Invalid solution;
-                        if k == j and pos[j]-1 < 1:
+                    if k == j and pos[j]-1 < 1:
                             continue
                         # Valid solution;
-                        else:
-                            solutions.add(tuple(sorted((i, j, k))))
+                    else:
+                        solutions.add(tuple(sorted((i, j, k))))
                     # If third number is '0' and we have zeros to use.
-                    elif k == 0 and zeros > 0:
-                        solutions.add(tuple(sorted((i, j, 0))))
+                elif k == 0 and zeros > 0:
+                    solutions.add(tuple(sorted((i, j, 0))))
 
             # Logic below is nearly identical logic as above. Kept it verbose for readibility.
 
             # Seeking negative numbers to offset postive numbers.
-            elif i > 0:
-                for j in neg:
-                    k = -i - j
-                    if k in neg:
-                        if k == j and neg[j]-1 < 1:
-                            continue
-                        else:
-                            solutions.add(tuple(sorted((i, j, k))))
-                    elif k == 0 and zeros > 0:
-                        solutions.add(tuple(sorted((i, j, 0))))
+        elif i > 0:
+            for j in neg:
+                k = -i - j
+                if k in neg:
+                    if k == j and neg[j]-1 < 1:
+                        continue
+                    else:
+                        solutions.add(tuple(sorted((i, j, k))))
+                 elif k == 0 and zeros > 0:
+                    solutions.add(tuple(sorted((i, j, 0))))
 
             # If we encounter a zero, check to see we have 3 or more.
-            elif zeros >= 3:
-                solutions.add((0, 0, 0))
+        elif zeros >= 3:
+            solutions.add((0, 0, 0))
 
-        return [list(s) for s in solutions]
-
+    return [list(s) for s in solutions]
 
 #### my solution: wrong answer not consider [0,0,0] condition
 
